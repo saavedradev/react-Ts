aaa84862e7f53fb2c8e3a383a375b0dbab5300fa
saavedradev <saavedra.dev@gmail.com>
2021-07-06 18:00:06 -0500

    changes to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Counter } from "./components/Counter";
+import { Usuario } from "./components/Usuario";
+
 function App() {
   return (
     <>
-      <h1>Hola Mundo - Ts</h1>
+      <h1>React + TypeScript</h1>
+      <hr/>
+
+      <Counter />
+      
+      <Usuario />
     </>
   );
 }
